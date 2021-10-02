fb2cb6edafa28ea87b32eb375edc13bb1629b70d
Jaime Eraso <jaime_eraso@hotmail.com>
2021-10-02 13:23:35 -0500

    Add configuration Sprint 3

--- public_html/js/validaciones.js
+++ public_html/js/validaciones.js
@@ -1,3 +1,5 @@
+let registros = [];
+
 function validar_nombre_usuario(string){
      valor = document.getElementById("in_nombre_usuario").value;
 	if (/^[a-zA-Z]{6,30}/.test(valor)){
@@ -12,7 +14,7 @@ function validar_contrasena(string){
   letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz0123456789",
   especiales = [8, 37, 39, 46],
   tecla_especial = false;
-  
+
 for (var i in especiales) {
   if (key == especiales[i]) {
     tecla_especial = true;
@@ -27,10 +29,32 @@ if (nombre.length<6){
   return false;
 }else{
   return true;
-}    
+}
 }
 function confirmar_contrasena(string){
-    
+
+}
+
+function agregarRegistro(){  /*Add function agregarRegistro jheb*/
+  var usuario = document.getElementById("in_nombre_usuario").value;
+  var contrasena = document.getElementById("in_contrasena").value;
+  var confirmar_contrasena = document.getElementById("in_confirmar_contrasena").value;
+  nuevoUsuario = [usuario, contrasena, confirmar_contrasena];
+  registros.push(nuevoUsuario);
+}
+
+
+function OrdenarArreglo(arreglo){  /*Add function OrdenarRegistro jheb*/
+  arreglo = arreglo.sort(function (a, b) {
+      if (a > b) {
+          return 1;
+      }
+      if (a< b) {
+          return -1;
+      }
+      return 0;
+  });
+   return arreglo;
 }
 
 module.exports.validar_nombre_usuario=validar_nombre_usuario;
